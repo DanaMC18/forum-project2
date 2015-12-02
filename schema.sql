42c275b0d7f72a4e4ca558a2900f4c6d3f457b91
Dana Czinsky <DanaCzinsky@gmail.com>
2015-12-01 22:15:43 -0500

    created schema, file structure, erd

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR,
+  username VARCHAR,
+  profile_pic TEXT
+);
+
+CREATE TABLE topics (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  content TEXT,
+  date DATE,
+  votes INTEGER,
+  comments INTEGER,
+  user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY,
+  content TEXT,
+  dateYes DATE,
+  likes INTEGER,
+  user_id INTEGER REFERENCES users(id),
+  topic_id INTEGER REFERENCES topic(id)
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  comment_id INTEGER REFERENCES comments(id)
+);
+
+CREATE TABLE votes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  topic_id INTEGER REFERENCES topics(id)
+);
